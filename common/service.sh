4433b446aef788d2796b188d194ee43c9e7a8489
联盟少侠 <1121114685@qq.com>
2018-10-15 18:47:24 +0800

    Update service.sh

--- common/service.sh
+++ common/service.sh
@@ -1,9 +1,9 @@
 #!/system/bin/sh
 #while [ "`getprop gsm.sim.operator.iso-country`" == "" ]; do
 #echo aaa
+sleep 45
+setprop gsm.sim.operator.iso-country us
+sleep 45
+setprop gsm.sim.operator.iso-country us
 sleep 90
-setprop gsm.sim.operator.iso-country us
-sleep 45
-setprop gsm.sim.operator.iso-country us
-sleep 45
 setprop gsm.sim.operator.iso-country us
